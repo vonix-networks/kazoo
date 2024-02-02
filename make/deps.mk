eb099e77fac024d84a0b4e349c5873fc214a98c8
khandu <54286273+khandun@users.noreply.github.com>
2024-02-03 02:34:48 +0530

    4.3 vonix pbx 20 (#9)
    * PBX-20:Dependency 'couchbeam' shows deprecation warnings

--- make/deps.mk
+++ make/deps.mk
@@ -77,7 +77,7 @@ dep_erlcloud = git https://github.com/erlcloud/erlcloud 3.2.4
 
 dep_erlazure = git https://github.com/lazedo/erlazure.git add-start-link
 
-dep_couchbeam = git https://github.com/2600hz/erlang-couchbeam 49521721a9c6d32512afa2ec27cdf310886eb779 # latest commit SHA to 2600hz branch
+dep_couchbeam = git https://github.com/2600hz/erlang-couchbeam 86d946646ad9dee1dee305b15d5c59e4c8e60cc2 # as per kazoo master branch commit SHA refered from 2600hz-kazoo5 branch
 ###dep_couchbeam = git https://github.com/benoitc/couchbeam 1.4.1
 ### waiting for pull requests
 ### https://github.com/benoitc/couchbeam/pull/158
