59870422f56fd03a78ca6947d49c5bf3ed6f26b0
khandun <khandun@alohatechnology.com>
2024-02-12 13:38:22 -0500

    PBX-24:Dependency 'escalus' has deprecation warnings

--- applications/fax/deps.mk
+++ applications/fax/deps.mk
@@ -1,6 +1,6 @@
 DEPS = cowboy escalus exml gen_smtp lager ra
 
-dep_escalus = git https://github.com/2600hz/erlang-escalus bb2d0bf0eb1115e7d277e7b2250cf37dcb72b8cb
+dep_escalus = git https://github.com/esl/escalus 4.2.11
 # used by fax cloud printer i think?
 
 dep_exml = git https://github.com/2600hz/erlang-exml 2.2.1
