c083d968a96beb991c9a95f54323ab6fb8388e5b
khandun <khandun@alohatechnology.com>
2024-02-21 07:10:38 -0500

    PBX-14: Removed unwanted changes

--- make/deps.mk
+++ make/deps.mk
@@ -1,4 +1,4 @@
-DEPS = amqp_client \
+DEPS ?= amqp_client \
 	certifi \
 	chatterbox \
 	couchbeam \
@@ -59,12 +59,12 @@ dep_certifi = hex 0.3.0
 dep_chatterbox = hex 0.8.0
 dep_eflame = git https://github.com/proger/eflame 34d0c19dee808d614d75d91c143386d5120efb4b
 dep_eiconv = git https://github.com/zotonic/eiconv
-# dep_escalus = git https://github.com/esl/escalus 4.2.11
+# dep_escalus = git https://github.com/esl/escalus 0de0463c345a1ade6fccfb9aadad719b58a1cef5
 # dep_exml = git https://github.com/paulgray/exml 2.2.1
 dep_jiffy = git https://github.com/lazedo/jiffy utf8  ## utf8 decode
 dep_meck = git https://github.com/eproxus/meck
 dep_qdate_localtime = git https://github.com/choptastic/qdate_localtime 1.2.1
-# dep_nklib = git https://github.com/NetComposer/nklib 1f29f8f566586618651e4680dcc488f2dc11ff5c # Latest SHA from nklib master branch
+dep_nklib = git https://github.com/NetComposer/nklib 1f29f8f566586618651e4680dcc488f2dc11ff5c # Latest SHA from nklib master branch
 dep_plists = hex 1.0.0
 
 dep_erlcloud = git https://github.com/erlcloud/erlcloud 3.2.4
