e4a6dbe89d5fc3ca8a1224ddd38814f68c6fc3a8
khandun <khandun@alohatechnology.com>
2024-02-21 06:59:04 -0500

    PBX-14: Fixed apns dependency issue at application level and removed dependencies from main deps.mk

--- make/deps.mk
+++ make/deps.mk
@@ -1,5 +1,4 @@
 DEPS = amqp_client \
-	apns \
 	certifi \
 	chatterbox \
 	couchbeam \
@@ -13,17 +12,12 @@ DEPS = amqp_client \
 	erlcloud \
 	erlsom \
 	erlydtl \
-	escalus \
-	exml \
-	fcm \
-	folsom \
 	fs_event \
 	fs_sync \
 	gen_smtp \
 	getopt \
 	gproc \
 	gun \
-	hep \
 	horse \
 	inet_cidr \
 	jesse \
@@ -39,7 +33,6 @@ DEPS = amqp_client \
 	recon \
 	reloader \
 	syslog \
-	trie \
 	wsock \
 	zucchini
 
@@ -58,7 +51,7 @@ dep_cowlib = git  https://github.com/ninenines/cowlib.git 2.12.1
 
 dep_gun = git https://github.com/2600hz/erlang-gun 2600hz-2.0.0-pre.3-4.3
 
-dep_apns = git https://github.com/2600hz/erlang-apns4erl.git 2600hz-2.4.2
+# dep_apns = git https://github.com/2600hz/erlang-apns4erl.git 2600hz-2.4.2
 
 
 dep_amqp_client = hex 3.7.14
@@ -66,12 +59,12 @@ dep_certifi = hex 0.3.0
 dep_chatterbox = hex 0.8.0
 dep_eflame = git https://github.com/proger/eflame 34d0c19dee808d614d75d91c143386d5120efb4b
 dep_eiconv = git https://github.com/zotonic/eiconv
-dep_escalus = git https://github.com/esl/escalus 4.2.11
-dep_exml = git https://github.com/paulgray/exml 2.2.1
+# dep_escalus = git https://github.com/esl/escalus 4.2.11
+# dep_exml = git https://github.com/paulgray/exml 2.2.1
 dep_jiffy = git https://github.com/lazedo/jiffy utf8  ## utf8 decode
 dep_meck = git https://github.com/eproxus/meck
 dep_qdate_localtime = git https://github.com/choptastic/qdate_localtime 1.2.1
-dep_nklib = git https://github.com/NetComposer/nklib 1f29f8f566586618651e4680dcc488f2dc11ff5c # Latest SHA from nklib master branch
+# dep_nklib = git https://github.com/NetComposer/nklib 1f29f8f566586618651e4680dcc488f2dc11ff5c # Latest SHA from nklib master branch
 dep_plists = hex 1.0.0
 
 dep_erlcloud = git https://github.com/erlcloud/erlcloud 3.2.4
@@ -91,7 +84,7 @@ dep_jesse = git https://github.com/2600hz/jesse 1.5-rc13
 ##dep_jesse = git https://github.com/for-GET/jesse 1.5.0-rc2
 
 dep_lager = git https://github.com/erlang-lager/lager 3.5.2
-dep_trie = git https://github.com/okeuday/trie v1.5.4
+# dep_trie = git https://github.com/okeuday/trie v1.5.4
 dep_fs_event = git https://github.com/jamhed/fs_event 783400da08c2b55c295dbec81df0d926960c0346
 dep_fs_sync = git https://github.com/jamhed/fs_sync 2cf85cf5861221128f020c453604d509fd37cd53
 dep_inet_cidr = git https://github.com/2600hz/erlang-inet_cidr.git 1.0.2
@@ -100,7 +93,7 @@ dep_erlang_localtime = git https://github.com/2600hz/erlang-localtime 2600hz
 
 
 ### need to update upstream ###
-dep_hep = git https://github.com/lazedo/hep 1.5.4
+# dep_hep = git https://github.com/lazedo/hep 1.5.4
 dep_ecsv = git https://github.com/lazedo/ecsv ecsv-1
 
 ### for scripts/dev-start-*.sh
@@ -115,7 +108,7 @@ dep_proper = git https://github.com/manopapad/proper v1.4
 
 dep_syslog = git https://github.com/2600hz/erlang-syslog bbad537a1cb5e4f37e672d2e2665659e850662d0
 
-dep_fcm = git https://github.com/softwarejoint/fcm-erlang.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
+# dep_fcm = git https://github.com/softwarejoint/fcm-erlang.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
 
 dep_gen_smtp = git https://github.com/2600hz/erlang-gen_smtp 3f80bfcd4fd8704739d264eb4d5005d4392f2a35
 ## pinning gen_smtp because upstream made some breaking changes (using maps in some options)
